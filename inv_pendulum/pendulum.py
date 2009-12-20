cb41bf582dbe7dd14a044779958ab462cad6c762
Norbert Braun <n.braun@ikp.uni-koeln.de>
2009-12-20 14:42:45 +0100

    inverse pendulum ODE simulation matching current version of robot firmware
    original bzr status:
    revno: 18
    committer: Norbert Braun <n.braun@ikp.uni-koeln.de>
    branch nick: xrp-1
    timestamp: Sun 2009-12-20 14:42:45 +0100

--- inv_pendulum/pendulum.py
+++ inv_pendulum/pendulum.py
@@ -6,32 +6,55 @@ import math
 def radToDeg(phi):
     return (phi / math.pi) * 180
 
-def desiredVelocity(t):
-    if t < 20:
-        return 1
-    elif t < 40:
-        return 0
-    elif t < 60:
-        return -1
-    else:
-        return 0
+# def desiredVelocity(t):
+#     if t < 20:
+#         return 1
+#     elif t < 40:
+#         return 0
+#     elif t < 60:
+#         return -1
+#     else:
+#         return 0
 
-def getForce(t, phi, phiDot, vCur):
-    vSet = desiredVelocity(t)
-    return (200.0 * (phi + 5 * phiDot + .1*(vCur - desiredVelocity(t))), 0., 0.)
+def motorResponse(speed):
+    if speed > 15:
+        return .05 * (speed - 15)
+    elif speed > -15:
+        return 0.
+    else:
+        return .05 * (speed + 15)
+
+error_int = 0
+def getMotor(t, phi, phiDot, pos, v):
+    # Units: [pos] = 1/4096 m
+    #        [phi] = (2*pi)/4096 rad = 1/652 rad
+
+    global error_int
+    
+    # phi += .01
+
+    # return .4 * phi + .2 * phiDot + 1.02 * v + .001 * pos
+    # error =  20. * (.4 * phi + .005 * pos + .4 * phiDot + 0.02 * v)
+    # error = 500 * phi + 50000 * phiDot + pos + 400 * v
+    error = 500 * phi + 35000 * phiDot + pos + 400 * v
+    error_int += error
+    
+    return (error + 3 * error_int) // 100000
+
+phiIni = .2
 
 world = ode.World()
 world.setGravity( (0., 0., -9.81) )
 
 ball = ode.Body(world)
 mb = ode.Mass()
-mb.setSphere(1000.0, 0.1)
+mb.setSphereTotal(.1, 0.1)
 ball.setMass(mb)
-ball.setPosition((0., 0., 1.))
+ball.setPosition((.35 * math.sin(phiIni), 0., .35 * math.cos(phiIni)))
 
 car = ode.Body(world)
 mc = ode.Mass()
-mc.setSphere(1000.0, 0.1)
+mc.setSphereTotal(1.0, 0.1)
 car.setMass(mc)
 car.setPosition((0., 0., 0.))
 
@@ -43,20 +66,29 @@ j1.setAxis((0., 1., 0. ))
 j2 = ode.SliderJoint(world)
 j2.attach(car, ode.environment)
 j2.setAxis((1., 0., 0.))
-
-ball.setPosition((math.sin(0), 0., math.cos(0)))
+j2.setParam(ode.ParamFMax, 5.)
 
 t = 0.0
-dt = 0.001
+dt = 0.01
 
-while t < 100.:
+oldPhi = 0
+oldX = 0
+
+while t < 100.0:
     x,y,z = car.getPosition()
     vx,vy,vz = car.getLinearVel()
-    phi = j1.getAngle()
+    phi = j1.getAngle() + phiIni
     phiDot = j1.getAngleRate()
-
-    car.addForce(getForce(t, phi, phiDot, vx))
-
+    
+    phiInt = int(phi*652.)
+    xInt = int(x * 4096.)
+    
+    mot = getMotor(t, phiInt, phiInt - oldPhi, xInt, xInt - oldX)
+    oldPhi = phiInt
+    oldX = xInt
+    
+    j2.setParam(ode.ParamVel, motorResponse(mot))
+    
     print "%.6f %.6f %.6f %.6f" % (t, x, vx, radToDeg(phi))
 
     world.step(dt)
